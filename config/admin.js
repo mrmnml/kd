7e526183cb6fe0ac78e2446daaec9deaf6725116
Lukas Achenbach <lukas.achenbach@dericon.de>
2025-01-05 01:39:23 +0100

    test

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
+  apiToken: {salt: env('API_TOKEN_SALT')}
 });
